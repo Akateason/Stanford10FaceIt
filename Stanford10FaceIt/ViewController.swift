03033c144231d5487494319e5185a353c93ca78a
谢天宸 <xietianchen@shimo.im>
2019-11-22 00:45:43 +0800

    Initial Commit

---
+++ Stanford10FaceIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stanford10FaceIt
+//
+//  Created by teason23 on 2019/11/22.
+//  Copyright © 2019 teason23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
